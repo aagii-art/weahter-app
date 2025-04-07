27fe143c5117f8c93cf66ea3054b85228d0c311f
aagii-art <altansvhaagii72@gmail.com>
2025-04-07 19:06:35 +0800

    4.7

---
+++ src/app/component/background.jsx
@@ -0,0 +1,11 @@
+
+
+
+export const Background = () => {
+    return (
+        <div className=" flex min-h-screen  bg-blue-500 " >
+            <div className=" w-1/2 bg-white  " ></div>
+            <div className=" w-1/2  bg-black   " ></div>
+        </div>
+    )
+}
---
+++ src/app/page.jsx
@@ -0,0 +1,15 @@
+import { Background } from "./component/background";
+import { Center } from "./component/center";
+
+ const  Home = () => {
+ 
+  return (
+    <>
+     <Background/>
+     <Center/>
+    </>
+  )
+}
+
+export default Home;
+
